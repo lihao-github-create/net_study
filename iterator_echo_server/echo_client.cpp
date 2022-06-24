f949712a95f5c578afeb92a59f31802cf7735774
lihao <13705579327@163.com>
2022-06-25 00:19:43 +0800

    test iterator_echo_server complete

--- iterator_echo_server/echo_client.cpp
+++ iterator_echo_server/echo_client.cpp
@@ -1,13 +1,64 @@
-  #include <cstdio>
-  #include <cstdlib>
-  #include <cstring>
-  #include <unistd.h>
-  #include <arpa/inet.h>
-  #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-  #define BUF_SIZE 1024
-  void error_handling(const char *message);
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
-  int main(int argc, const char *argv[]) {
-    return 0;
+#define BUF_SIZE 1024
+void error_handling(const char *message);
+
+int main(int argc, const char *argv[]) {
+  if (argc != 3) {
+    printf("Usage : %s <IP> <port> \n", argv[0]);
+    exit(1);
   }
+  char message[BUF_SIZE];
+  int sock;
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    error_handling("socket() error");
+  }
+
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port = htons(atoi(argv[2]));
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    error_handling("connect() error");
+  } else {
+    puts("Connected...");
+  }
+  int str_len = 0;
+  while (1) {
+    fputs("Input message(Q to quit) : ", stdout);
+    fgets(message, BUF_SIZE, stdin);
+    if (!strcmp(message, "q\n") || !strcmp(message, "Q\n")) {
+      break;
+    }
+
+    str_len = write(sock, message, strlen(message));
+    printf("write message length = %d\n", str_len);
+    int read_len = 0;
+    int remain = str_len;
+    printf("remain = %d\n", remain);
+    while (remain > 0 && (read_len = read(sock, message, BUF_SIZE))) {
+      remain -= str_len;
+      printf("remain = %d\n", remain);
+    }
+    message[str_len] = 0;
+    printf("Message from server: %s", message);
+  }
+  close(sock);
+
+  return 0;
+}
+
+void error_handling(const char *message) {
+  fputs(message, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
--- iterator_echo_server/echo_server.cpp
+++ iterator_echo_server/echo_server.cpp
@@ -9,7 +9,7 @@
 #define BUF_SIZE 1024
 void error_handling(const char *message);
 
-int main(int argc, char *argv[]) { 
+int main(int argc, const char *argv[]) {
   if (argc != 2) {
     printf("Usage : %s <port>\n", argv[0]);
     exit(1);
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htonl(atoi(argv[1]));
+  serv_addr.sin_port = htons(atoi(argv[1]));
 
   if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
     error_handling("bind() error");
@@ -40,23 +40,26 @@ int main(int argc, char *argv[]) {
   socklen_t clnt_addr_sz;
   int count = 0;
   while (1) {
-    int clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_sz);
+    int clnt_sock =
+        accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_sz);
     if (clnt_sock == -1) {
       error_handling("accept() error");
     } else {
       printf("Connected client %d \n", count++);
     }
     int str_len = 0;
-    while ((str_len == read(clnt_sock, message, BUF_SIZE)) != 0) {
+    while ((str_len = read(clnt_sock, message, BUF_SIZE)) != 0) {
+      // 正常情况下，会一直服务到client首先关闭连接
+      printf("length of message = %d\n", str_len);
       write(clnt_sock, message, str_len);
     }
     close(clnt_sock);
   }
   close(serv_sock);
-  return 0; 
+  return 0;
 }
 
-void error_handling(const char* message) {
+void error_handling(const char *message) {
   fputs(message, stderr);
   fputc('\n', stderr);
   exit(1);
